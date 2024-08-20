d5f518c09ad1f787e8fc1b9c24f30f86278478be
Youngeun <37464219+JYoungeun@users.noreply.github.com>
2024-08-20 17:34:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,31 +9,18 @@ const checkForPalindrome = input => {
       alert('Please input a value');
       return;
     }
-    // if (input === ''): Checks if input is empty. If so, alert and exit function.
   
     resultText.replaceChildren(); 
-    // Clears any existing content inside `resultText`
   
     const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
-    // Creates new string from 'input', removes non letters/numbers, then converts lowercase.
 
     let resultMsg = `<strong>${originalInput}</strong> ${
       lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
     } a palindrome.`;
-    // lowerCaseStr === [...lowerCaseStr].reverse().join('') checks if same as reverse.
-    // join('') converts the reversed array back into a string.
-    // If reversed string === original, adds "is" to message, otherwise "is not".
 
-    const pTag = document.createElement('p');
-    pTag.className = 'user-input';
-    pTag.innerHTML = resultMsg;
-    resultText.appendChild(pTag);
-    // Creates new p element pTag, sets class to user-input, and inerts resultMsg.
-    // Appends new p to resultText to display result on page.
+    resultText.innerHTML = `<p>${resultMsg}</p>`;
   
     resultText.classList.remove('hidden');
-    // Removes the hidden class from resultText to ensure it is visible on the page.
-  };
 
 palindromeCheck.addEventListener("click", () => {
     checkForPalindrome(userInput.value);
